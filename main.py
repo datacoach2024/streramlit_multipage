f86976f62edc8c7e69c606f51ce3570dcef0f8ae
Maruf <datacoach2024@gmail.com>
2024-11-15 18:36:51 +0500

    use second method to create pages

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import plotly
 import plotly.express as px
 import streamlit as st
 
-st.title('Main page')
+# st.title('Main page')
+st.logo('static/logo.png')
+
+
+pages = [
+    st.Page('sales.py', title = 'Sales Report'),
+    st.Page('customers.py', title = 'Customers Report'),
+    st.Page('products.py', title = 'Products Report')
+]
+
+pg = st.navigation(pages)
+pg.run()
+
