41fecfa7ccc74ebd88b1d04303be7b322e06e1c5
Maruf <datacoach2024@gmail.com>
2024-11-15 19:11:15 +0500

    add scatter chart to sales.py

--- sales.py
+++ sales.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import backend
+import plotly.express as px
+
 
 st.title("Sales Report")
+
+tips = backend.load_tips_data()
+
+fig = px.scatter(
+    data_frame = tips,
+    x = 'tip',
+    y = 'total_bill'
+)
+
+st.plotly_chart(fig)
